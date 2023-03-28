57848b240b98eada87934eaad04f28c8944ae1b2
X-Engineer-001 <fabin1790@gmail.com>
2023-03-28 16:55:44 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <p>TEST</p><p>TEST2</p>
+        <p>TEST</p>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
